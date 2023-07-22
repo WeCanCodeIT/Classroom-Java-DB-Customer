a0d46b0b1767b0f0188082e503fc76368286c330
John Moseley <johnmoseley@Wecancodeit.org>
2023-07-22 08:45:57 -0400

    First Commit

---
+++ src/main/java/com/customer/accessingdatajpa/AccessingdatajpaApplication.java
@@ -0,0 +1,15 @@
+package com.customer.accessingdatajpa;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingdatajpaApplication.class, args);
+	}
+
+	
+}
